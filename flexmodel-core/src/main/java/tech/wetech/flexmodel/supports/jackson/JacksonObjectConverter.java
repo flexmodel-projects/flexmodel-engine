0296c2e6e16393290d4ec268263cb964ab51d3ea
Cheng Jinbao <cjbi@outlook.com>
2024-08-19 18:57:43 +0800

    feat: graphql query operation

--- flexmodel-core/src/main/java/tech/wetech/flexmodel/supports/jackson/JacksonObjectConverter.java
+++ flexmodel-core/src/main/java/tech/wetech/flexmodel/supports/jackson/JacksonObjectConverter.java
@@ -43,7 +43,7 @@ public class JacksonObjectConverter implements JsonObjectConverter {
   @Override
   public String toJsonString(Object obj) {
     try {
-      return jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+      return jsonMapper.writeValueAsString(obj);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
