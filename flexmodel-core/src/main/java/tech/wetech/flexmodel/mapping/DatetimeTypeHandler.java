7293d0a95c28db42d6e03bdd7ba0d4d2fb7473aa
Cheng Jinbao <cjbi@outlook.com>
2025-02-08 09:39:57 +0800

    feat: 优化日期时间转换

--- flexmodel-core/src/main/java/tech/wetech/flexmodel/mapping/DatetimeTypeHandler.java
+++ flexmodel-core/src/main/java/tech/wetech/flexmodel/mapping/DatetimeTypeHandler.java
@@ -1,6 +1,8 @@
 package tech.wetech.flexmodel.mapping;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 /**
  * @author cjbi
@@ -14,6 +16,13 @@ public class DatetimeTypeHandler implements TypeHandler<LocalDateTime> {
     if (value instanceof LocalDateTime localDateTime) {
       return localDateTime;
     }
-    return LocalDateTime.parse(value.toString());
+    // 使用 DateTimeFormatterBuilder 创建解析器
+    DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+      .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+      .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+      .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS"))
+      .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"))
+      .toFormatter();
+    return LocalDateTime.parse(value.toString(), formatter);
   }
 }
